12ad41d3dcf31d451cdb943e6784e38984640b6c
hengin-eer <hirokix0608@gmail.com>
2024-08-29 00:37:15 +0900

    fix: リンタによる修正

--- src/components/DisplayPoints.tsx
+++ src/components/DisplayPoints.tsx
@@ -1,25 +1,19 @@
-import { GameDataType } from "./Calculator";
+import { GameDataType } from './Calculator';
 
-function DisplayPoints({
-    Points,
-    currentUser,
-}: GameDataType) {
-    return (
-        <div className="border border-black py-5 grid place-items-center">
-            <p className="text-xl mb-2">ターン: {currentUser}</p>
+function DisplayPoints({ Points, currentUser }: GameDataType) {
+	return (
+		<div className="grid py-5 border border-black place-items-center">
+			<p className="mb-2 text-xl">ターン: {currentUser}</p>
 
-            <div className="py-4 flex gap-4">
-                {Points.binaryResult.map((bitValue) => (
-                    <span className="text-6xl">{bitValue}</span>
-                ))}
-            </div>
-
-            <p className="text-xl">
-                <span className="text-3xl mr-2">{Points.decimalResult}</span>
-                ポイント
-            </p>
-        </div>
-    )
-};
-
+			<div className="flex gap-4 py-4">
+				{Points.binaryResult.map((bitValue) => (
+					<span className="text-6xl">{bitValue}</span>
+				))}
+			</div>
+			<p className="text-xl">
+				<span className="mr-2 text-3xl">{Points.decimalResult}</span>ポイント
+			</p>
+		</div>
+	);
+}
 export default DisplayPoints;
